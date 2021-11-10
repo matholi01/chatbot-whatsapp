3fbba68427338bfbb6a1e26751a815f9c61650d1
silvatheus01 <theusoliveira01@dcc.ufrj.br>
2021-11-10 18:41:00 -0300

    Retorno do método GET alterado. Os eventos são filtrados pelo número da semana.

--- django/programacao/views.py
+++ django/programacao/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -5,11 +6,26 @@ from rest_framework.decorators import api_view
 from .models import Evento
 from .serializers import *
 
-from django.db.models import Q
-
 @api_view(['GET'])
 def programacao_list(request):
     if request.method == 'GET':
-        dados = Evento.objects.all()
-        serializer = EventoSerializer(dados, context={'request': request}, many=True)
-        return Response(serializer.data)
+        # Lista com os eventos de um dia da semana
+        eventos_dia_semana = []
+
+        # Lista de eventos de um dia da semana "serializados"
+        serializer = []
+
+        # Lista com todos os eventos da semana serializados
+        resposta = []
+
+        # Eventos da semana atual
+        dados_semana_atual = Evento.objects.filter(data__week = datetime.today().isocalendar()[1])
+        
+        for i,n in enumerate(list(range(1,8))):
+            # Filtra a partir do número da dia da semana. Começa na segunda e termina no domingo.
+            eventos_dia_semana.append(dados_semana_atual.filter(data__week_day=n%7+1)) 
+
+            serializer.append(EventoSerializer(eventos_dia_semana[i], context={'request': request}, many=True).data)
+            resposta.append(serializer[i])
+    
+        return Response(resposta)
