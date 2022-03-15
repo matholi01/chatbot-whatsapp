39224d5af5f0d433b2f62bf19437e4699ae9a3d1
silvatheus01 <theusoliveira01@dcc.ufrj.br>
2022-03-15 16:13:31 -0300

    Model Igreja adicionado

--- django/programacao/admin.py
+++ django/programacao/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Evento
+from .models import Evento, Igreja
 
 # Register your models here.
 
 admin.site.register(Evento)
+admin.site.register(Igreja)
+
--- django/programacao/models.py
+++ django/programacao/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+class Igreja(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+
+    def __str__(self):
+        return self.nome
+
 class Evento(models.Model):
     nome = models.CharField('Nome', max_length=50)
     data = models.DateTimeField('Data')
+    igreja = models.ForeignKey('Igreja', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nome + ' | ' + self.data.strftime("%d/%m - %Hh%M")
+        return self.nome + ' | ' + self.data.strftime("%d/%m - %Hh%M") + ' | ' + self.igreja.nome
+
