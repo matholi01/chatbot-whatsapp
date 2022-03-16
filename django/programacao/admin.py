c296d58d5cbc580c90db4f52b6a4441372400d75
silvatheus01 <theusoliveira01@dcc.ufrj.br>
2022-03-16 19:12:34 -0300

    Campo "igreja" adicionado aos dados dos usuários

--- django/programacao/admin.py
+++ django/programacao/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from .models import Evento, Igreja
+from .models import Evento, Igreja, Administrador
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
 
 admin.site.register(Evento)
 admin.site.register(Igreja)
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class AdministradorInline(admin.StackedInline):
+    model = Administrador
+    can_delete = False
+    verbose_name_plural = 'Administrador'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (AdministradorInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
--- django/programacao/models.py
+++ django/programacao/models.py
@@ -1,16 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# Representa uma igreja do campus ADMJI
 class Igreja(models.Model):
     nome = models.CharField('Nome', max_length=50)
 
     def __str__(self):
         return self.nome
 
+# Representa um evento na programação
 class Evento(models.Model):
     nome = models.CharField('Nome', max_length=50)
     data = models.DateTimeField('Data')
+    # Todo evento é relacionado a uma igreja especifica
+    # Campo devido a relação "One-To-Many" (Um para muitos) com o model Igreja
     igreja = models.ForeignKey('Igreja', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome + ' | ' + self.data.strftime("%d/%m - %Hh%M") + ' | ' + self.igreja.nome
+
+
+class Administrador(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Todo administrador é relacionado a uma igreja especifica
+    # Campo devido a relação "One-To-Many" (Um para muitos) com o model Igreja
+    igreja = models.ForeignKey('Igreja', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.igreja.nome
 
