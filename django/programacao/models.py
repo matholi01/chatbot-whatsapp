3b61f4e089680a3448a6f8dbfb4ba6abf3570d5c
silvatheus01 <theusoliveira01@dcc.ufrj.br>
2022-03-21 14:10:54 -0300

    Textos de ajuda adicionados aos campos do model "Evento"

--- django/programacao/models.py
+++ django/programacao/models.py
@@ -10,11 +10,13 @@ class Igreja(models.Model):
 
 # Representa um evento na programação
 class Evento(models.Model):
-    nome = models.CharField('Nome', max_length=50)
+    nome = models.CharField('Nome', max_length=50,
+    help_text='Nome do evento. Ex: "Culto de Adoração".')
     data = models.DateTimeField('Data')
     # Todo evento é relacionado a uma igreja especifica
     # Campo devido a relação "One-To-Many" (Um para muitos) com o model Igreja
-    igreja = models.ForeignKey('Igreja', on_delete=models.CASCADE)
+    igreja = models.ForeignKey('Igreja', on_delete=models.CASCADE,
+    help_text='Igreja onde o evento ocorrerá.')
 
     def __str__(self):
         return self.nome + ' | ' + self.data.strftime("%d/%m - %Hh%M") + ' | ' + self.igreja.nome
